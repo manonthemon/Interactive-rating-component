0ccec5d43a5e427040c720cd4838d09283394922
Manonthemon <klimowicz.maciej@gmail.com>
2023-04-04 20:06:37 +0100

    Added thank you card, started working on JS logic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("HELLO!")
+let ratingCard = document.getElementById("rating-card");
+let thankyouCard = document.getElementById("thankyou-card");
+let submitButton = document.getElementById("submit");
+
+
+
+submitButton.addEventListener('click', function() {
+    submitClick()
+  });
+
+
+  function submitClick() {
+    ratingsCard.style.display = "none";
+    thankyouCard.classList.remove("hidden");
+    // questionText.textContent = quizQuestions[0].question;
+    // questionsContainer.append(questionText);
+};
